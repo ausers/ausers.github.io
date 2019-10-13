9cad59c07dbbfafe20989217ea40185374269050
ausers <54773815+ausers@users.noreply.github.com>
2019-10-14 00:20:36 +0800

    Add files via upload

---
+++ files/ssrspeed一键.bat
@@ -0,0 +1,28 @@
+@echo off&
+echo ##### 请放在ssrspeed目录下运行 #####
+echo.
+:start
+echo 1：开始测速
+echo 2：安装pip
+echo 请选择（1/2）：
+choice /c 12
+if %errorlevel%==2 (goto :pip)
+if %errorlevel%==1 (goto :test)
+
+:pip
+python -m pip install --upgrade pip
+pip3 install -r requirements.txt
+pip3 install pillow
+pip3 install requests
+pip3 install pysocks
+pause
+&& goto :start
+
+:test
+set /p a=请输入您的订阅链接:
+if "%a%"=="" (
+goto :test
+) else (
+python main.py -u "%a%"
+)
+pause
