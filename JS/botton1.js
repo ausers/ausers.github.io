9848ca3bfcd865542d56e89445d394a13a7f5aa9
ausers <54773815+ausers@users.noreply.github.com>
2019-12-25 01:24:49 +0800

    Update botton1.js

--- JS/botton1.js
+++ JS/botton1.js
@@ -6,7 +6,7 @@ function myFunction1() {
         div1.style.display = "none";
     }
     var x;
-    for (var i = 1; i <= 4; i++) {
+    for (var i = 1; i <= 10; i++) {
         if (i == 1) {
             continue;
         } else {
@@ -24,7 +24,7 @@ function myFunction2() {
         div2.style.display = "none";
     }
     var x;
-    for (var i = 1; i <= 4; i++) {
+    for (var i = 1; i <= 10; i++) {
         if (i == 2) {
             continue;
         } else {
@@ -42,7 +42,7 @@ function myFunction3() {
         div3.style.display = "none";
     }
     var x;
-    for (var i = 1; i <= 4; i++) {
+    for (var i = 1; i <= 10; i++) {
         if (i == 3) {
             continue;
         } else {
@@ -60,8 +60,116 @@ function myFunction4() {
         div4.style.display = "none";
     }
     var x;
-    for (var i = 1; i <= 4; i++) {
+    for (var i = 1; i <= 10; i++) {
         if (i == 4) {
+            continue;
+        } else {
+            x = document.getElementById("myDIV"+i);
+            x.style.display = "none";
+        }
+    }
+}
+
+function myFunction5() {
+    var div5 = document.getElementById("myDIV5");
+    if (div5.style.display === "none") {
+        div5.style.display = "block";
+    } else {
+        div5.style.display = "none";
+    }
+    var x;
+    for (var i = 1; i <= 10; i++) {
+        if (i == 5) {
+            continue;
+        } else {
+            x = document.getElementById("myDIV"+i);
+            x.style.display = "none";
+        }
+    }
+}
+
+function myFunction6() {
+    var div6 = document.getElementById("myDIV6");
+    if (div6.style.display === "none") {
+        div6.style.display = "block";
+    } else {
+        div6.style.display = "none";
+    }
+    var x;
+    for (var i = 1; i <= 10; i++) {
+        if (i == 6) {
+            continue;
+        } else {
+            x = document.getElementById("myDIV"+i);
+            x.style.display = "none";
+        }
+    }
+}
+
+function myFunction7() {
+    var div7 = document.getElementById("myDIV7");
+    if (div7.style.display === "none") {
+        div7.style.display = "block";
+    } else {
+        div7.style.display = "none";
+    }
+    var x;
+    for (var i = 1; i <= 10; i++) {
+        if (i == 7) {
+            continue;
+        } else {
+            x = document.getElementById("myDIV"+i);
+            x.style.display = "none";
+        }
+    }
+}
+
+function myFunction8() {
+    var div8 = document.getElementById("myDIV8");
+    if (div8.style.display === "none") {
+        div8.style.display = "block";
+    } else {
+        div8.style.display = "none";
+    }
+    var x;
+    for (var i = 1; i <= 10; i++) {
+        if (i == 8) {
+            continue;
+        } else {
+            x = document.getElementById("myDIV"+i);
+            x.style.display = "none";
+        }
+    }
+}
+
+function myFunction9() {
+    var div9 = document.getElementById("myDIV9");
+    if (div9.style.display === "none") {
+        div9.style.display = "block";
+    } else {
+        div9.style.display = "none";
+    }
+    var x;
+    for (var i = 1; i <= 10; i++) {
+        if (i == 9) {
+            continue;
+        } else {
+            x = document.getElementById("myDIV"+i);
+            x.style.display = "none";
+        }
+    }
+}
+
+function myFunction10() {
+    var div10 = document.getElementById("myDIV10");
+    if (div10.style.display === "none") {
+        div10.style.display = "block";
+    } else {
+        div10.style.display = "none";
+    }
+    var x;
+    for (var i = 1; i <= 10; i++) {
+        if (i == 10) {
             continue;
         } else {
             x = document.getElementById("myDIV"+i);
